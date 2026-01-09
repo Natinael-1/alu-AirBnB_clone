b9014fffd7617dcb8fe7948ab50c693da3e4f5db
Natinael-1 <n.borana@alustudent.com>
2026-01-09 11:51:26 -0800

    Modified file

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+printf '#!/usr/bin/python3
 """
 This module defines the BaseModel class.
 """
@@ -10,26 +10,26 @@ class BaseModel:
     BaseModel defines all common attributes/methods for other classes.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
-        Initializes a new BaseModel instance.
+        Initializes a new instance.
         """
-        # Assign a random UUID (converted to string)
-        self.id = str(uuid.uuid4())
-        
-        # Assign the current date and time
-        self.created_at = datetime.now()
-        
-        # Assign the update time (initially same as creation)
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(value, "%%Y-%%m-%%dT%%H:%%M:%%S.%%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
         Returns a string representation of the instance.
-        Format: [<class name>] (<self.id>) <self.__dict__>
         """
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
@@ -39,18 +39,11 @@ class BaseModel:
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__ of the instance.
+        Returns a dictionary containing all keys/values of __dict__.
         """
-        # 1. Create a COPY of the dictionary so we don't mess up the original object
         new_dict = self.__dict__.copy()
-        
-        # 2. Add the class name to the dictionary
         new_dict["__class__"] = self.__class__.__name__
-        
-        # 3. Convert the datetime objects to ISO format strings
-        # We use isoformat() because JSON cannot understand Python 'datetime' objects,
-        # but it understands Strings perfectly.
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
-        
         return new_dict
+' > models/base_model.py
