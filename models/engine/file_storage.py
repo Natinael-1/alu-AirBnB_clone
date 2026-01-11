353567789dbdc4366c64e80bf3d2863f5d954182
Natinael-1 <n.borana@alustudent.com>
2026-01-11 05:21:51 -0800

    Created files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,7 @@ This module defines the FileStorage class.
 import json
 import os
 
+
 class FileStorage:
     """
     Serializes instances to a JSON file and deserializes JSON file to instances.
@@ -34,7 +35,7 @@ class FileStorage:
         # Convert all objects in memory to dictionary representations
         for key, obj in FileStorage.__objects.items():
             obj_dict[key] = obj.to_dict()
-        
+
         # Write the dictionary to the file in JSON format
         with open(FileStorage.__file_path, "w") as f:
             json.dump(obj_dict, f)
@@ -46,16 +47,26 @@ class FileStorage:
         """
         from models.base_model import BaseModel
         from models.user import User
-        
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
         classes = {
             "BaseModel": BaseModel,
-            "User": User
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
         }
-        
+
         if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, "r") as f:
                 obj_dict = json.load(f)
-                
+
             for key, value in obj_dict.items():
                 class_name = value["__class__"]
                 if class_name in classes:
---
+++ models/state.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This module defines the State class """
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """ Represents a State """
+    name = ""
